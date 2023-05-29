22fcf7a484c254637b997c23b1d8e495f6fd41d5
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-29 11:35:23 +0200

    Added f between vecs function

--- notebooks/src/utils.py
+++ notebooks/src/utils.py
@@ -12,3 +12,17 @@ def create_heatmap(x, y, f, f_is_vectorized = True):
             for j, y_val in enumerate(y):
                 Z[j, i] = f(x_val, y_val)
     return Z
+
+
+def f_between_vectors(v1, v2, f, num_points, pad_factor = 0):
+    thetas = np.linspace(0-pad_factor, 1 + pad_factor, num_points)
+    values = np.zeros((num_points,), dtype = float)
+    for i, theta in enumerate(thetas):
+        v = theta*v1+(1-theta)*v2
+        values[i] = f(v)
+    return v
+
+
+
+
+
