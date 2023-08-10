d511e444a9867d252bb8c5ca090b7896e8c38531
allavinner <joel.w.ottosson@gmail.com>
2023-08-10 20:46:30 +0200

    Prediction map

--- scripts/prediction_map.py
+++ scripts/prediction_map.py
@@ -16,8 +16,8 @@ dy = (box_y1 - box_y0)/ny
 
 x_axis = torch.linspace(box_x0, box_x1, nx)
 y_axis = torch.linspace(box_y0, box_y1, ny)
-X, Y = torch.meshgrid((y_axis, x_axis))
-points = torch.stack((X.flatten(), Y.flatten())).T
+Y, X = torch.meshgrid((y_axis, x_axis))
+points = torch.stack((X, Y))
 y_axis.shape
 x_axis.shape
 X.shape
@@ -33,8 +33,7 @@ X.shape
 # input = torch.randn(2, 3)
 # output = m(input)
 
-
-Z = points.pow(2).sum(1).reshape((nx, ny)).T
+Z = points.pow(2).sum(0)
 
 fig = go.Figure(
     data = go.Heatmap(
---
+++ tmp.py
@@ -0,0 +1,22 @@
+import plotly.graph_objects as go
+
+fig = go.Figure()
+
+fig.add_trace(go.Bar(
+    x = [['First', 'First', 'Second', 'Second'],
+         ["A", "B", "A", "B"]],
+    y = [8, 3, 6, 5],
+    name = "Children",
+))
+
+fig.add_trace(go.Bar(
+    x = [['First', 'First', 'Second', 'Second'],
+         ["A", "B", "A", "B"]],
+    y = [2, 3, 1, 5],
+    name = "Adults",
+))
+
+
+fig.update_layout(title_text="Multi-category axis")
+
+fig.show()
