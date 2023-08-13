918d68c234598075c0d2a62bb45e135339388bab
allavinner <joel.w.ottosson@gmail.com>
2023-08-13 20:45:36 +0200

    fixed reshape of flatt point prediciton map

--- scripts/prediction_map.py
+++ scripts/prediction_map.py
@@ -1,33 +1,34 @@
 import torch
 import plotly
 import plotly.graph_objects as go
-        
 
 
-def create_prediction_map(transform,
-                          box_x0 = -1.,
-                          box_y0 = 4.,
-                          box_x1 = 1.,
-                          box_y1 = -4.,
-                          nx = 500,
-                          ny = 1000
-                          ):
+
+box_x0 = -3.
+box_y0 = 4.
+box_x1 = 3.
+box_y1 = -4.
+nx = 500
+ny = 1000
 
 
-    dx = (box_x1 - box_x0)/nx
-    dy = (box_y1 - box_y0)/ny
+x_axis = torch.linspace(box_x0, box_x1, nx)
+y_axis = torch.linspace(box_y0, box_y1, ny)
+Y, X = torch.meshgrid(y_axis, x_axis)
+points = torch.stack((X, Y)).reshape((2, -1)).T
 
-    x_axis = torch.linspace(box_x0, box_x1, nx)
-    y_axis = torch.linspace(box_y0, box_y1, ny)
-    Y, X = torch.meshgrid((y_axis, x_axis))
-    points = torch.stack((X, Y))
-    
-    Z = transform(torch.stack(X,Y))
+transform = lambda x: x.pow(2).sum(1)
 
-    fig = go.Figure(
-        data = go.Heatmap(
-            z = Z,
-            x = x_axis,
-            y = y_axis
-        )
-    )
+t_points = transform(points)
+
+Z = t_points.reshape((ny, nx))
+
+fig = go.Figure(
+    data = go.Heatmap(
+        z = Z,
+        x = x_axis,
+        y = y_axis
+     )
+)
+fig.show()
+
