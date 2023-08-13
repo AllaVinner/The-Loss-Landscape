583a61c4027b95e52415af719058502697466f03
allavinner <joel.w.ottosson@gmail.com>
2023-08-13 20:54:20 +0200

    Implemented create prediction map function

--- scripts/prediction_map.py
+++ scripts/prediction_map.py
@@ -3,32 +3,43 @@ import plotly
 import plotly.graph_objects as go
 
 
+def create_prediciton_map(transform,
+                          box = None,
+                          nx = 500,
+                          ny = 500):
+    if box is None:
+        box = dict()
+    if not 'x0' in box:
+        box['x0'] = -2
+    if not 'x1' in box:
+        box['x1'] = 2
+    if not 'y0' in box:
+        box['y0'] = -2
+    if not 'y1' in box:
+        box['y1'] = 2
 
-box_x0 = -3.
-box_y0 = 4.
-box_x1 = 3.
-box_y1 = -4.
-nx = 500
-ny = 1000
+    x_axis = torch.linspace(box['x0'], box['x1'], nx)
+    y_axis = torch.linspace(box['y0'], box['y1'], ny)
+    Y, X = torch.meshgrid(y_axis, x_axis)
+    points = torch.stack((X, Y)).reshape((2, -1)).T
 
 
-x_axis = torch.linspace(box_x0, box_x1, nx)
-y_axis = torch.linspace(box_y0, box_y1, ny)
-Y, X = torch.meshgrid(y_axis, x_axis)
-points = torch.stack((X, Y)).reshape((2, -1)).T
+    t_points = transform(points)
 
-transform = lambda x: x.pow(2).sum(1)
+    Z = t_points.reshape((ny, nx))
 
-t_points = transform(points)
+    return Z, x_axis, y_axis
 
-Z = t_points.reshape((ny, nx))
+
+Z, x_axis, y_axis = create_prediciton_map(lambda x: torch.sin(5*x.pow(2).sum(1)),
+                                          box={'x0' : -5})
 
 fig = go.Figure(
     data = go.Heatmap(
         z = Z,
         x = x_axis,
         y = y_axis
-     )
+    )
 )
-fig.show()
 
+fig.show()
